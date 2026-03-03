adcaa3291c7551d58c9f94f12dd905049c79f051
RG <rampage1337pwnz@gmail.com>
2026-03-03 13:11:29 +0300

    finished profile layout

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -28,23 +28,57 @@ const ProfilePage = () => {
       <SearchInput onSelectProfile={handleProfileSelect} />
       {user && (
         <div className="user-profile">
-          <img
-            className="profile-avatar"
-            src={user.avatar_url}
-            alt="user avatar"
-          />
-          <div className="profile-stats">
-            <div className="profile-followers"></div>
-            <div className="profile-following"></div>
-            <div className="profile-location"></div>
+          {console.log(user)}
+          <div className="profile-top">
+            <img
+              className="profile-avatar"
+              src={user.avatar_url}
+              alt="user avatar"
+            />
+            <div className="profile-stats">
+              <div className="profile-followers">
+                <span className="profile-followers-text">Folowers</span>
+                <span className="profile-followers-count">
+                  {user.followers}
+                </span>
+              </div>
+              <div className="profile-following">
+                <span className="profile-following-text">Folowing</span>
+                <span className="profile-following-count">
+                  {user.following}
+                </span>
+              </div>
+              <div className="profile-location">
+                <span className="profile-location-text">Location</span>
+                <span className="profile-location-info">{user.location}</span>
+              </div>
+            </div>
           </div>
           <div className="profile-info">
             <h2 className="profile-name">{user.name}</h2>
-            <p className="search-result-description">
+            <p className="profile-description">
               {user.bio || "No bio available"}
             </p>
           </div>
-          <div className="profile-repos"></div>
+          <div className="profile-repos">
+            {Array(4)
+              .fill(null)
+              .map((_, index) => (
+                <div key={index} className="profile-repo-card">
+                  <h3 className="repo-card-header">Card Header</h3>
+                  <p className="repo-card-info">Card description</p>
+                  <div className="repo-card-stats">
+                    <span className="repo-card-organisation">#</span>
+                    <span className="repo-card-clones">#</span>
+                    <span className="repo-card-stars">#</span>
+                    <p className="repo-card-updated">updated # days ago</p>
+                  </div>
+                </div>
+              ))}
+          </div>
+          <div className="profile-view-all-repos">
+            <button className="view-repos-button">View all repositories</button>
+          </div>
         </div>
       )}
     </div>
